9bdb4692ff2503428da29b6aa205e479ae65df1c
Y0ungo <ridiculouszero@ewha.ac.kr>
2025-09-06 03:12:25 +0900

    ✨ feat: 스터디(기간, 계획-챌린지 수) 개설

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Homepage";
 import Challenge from "./pages/study_subject";
 import Money from "./pages/study_money"
+import Duration from "./pages/study_duration"
+import How from "./pages/study_how"
 
 const App = () => {
   return (
@@ -14,8 +16,12 @@ const App = () => {
 
         {/* 스터디 생성 - 과목 */}
         <Route path="/study/new/subject" element={<Challenge />} />
-        {/* 스터디 생성 - 금액 */}
+        {/* 스터디 생성 - 금액과 인원 */}
         <Route path="/study/new/money" element={<Money />} />
+        {/* 스터디 생성 - 기간 */}
+        <Route path="/study/new/duration" element={<Duration />} />
+        {/* 스터디 생성 - 계획/방법(how) */}
+        <Route path="/study/new/how" element={<How />} />
       </Routes>
     </Router>
   );
