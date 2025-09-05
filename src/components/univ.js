510f88b69e59cb80076058fdccaabae1f91bace1
Y0ungo <ridiculouszero@ewha.ac.kr>
2025-09-05 20:52:01 +0900

    🔧 chore: 임의 기능 생성

---
+++ src/components/univ.js
@@ -0,0 +1,7 @@
+// 신촌 4개 대학 목록
+export const SINCHEON_UNIVS = [
+  { value: "sogang", label: "서강대학교" },
+  { value: "yonsei", label: "연세대학교" },
+  { value: "ewha",  label: "이화여자대학교" },
+  { value: "hongik",label: "홍익대학교" },
+];
