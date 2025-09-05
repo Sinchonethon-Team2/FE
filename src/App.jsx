9cefb926fea9fb7c2263a537d37907374d8e1a93
Y0ungo <ridiculouszero@ewha.ac.kr>
2025-09-06 02:14:56 +0900

    Fix case: nextButton -> NextButton

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 // src/App.js
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import StudyHome from "./pages/Homepage";
+import Home from "./pages/Homepage";
+import Challenge from "./pages/study_subject";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        {/* 메인 페이지 */}
-        <Route path="/" element={<StudyHome />} />
+        {/* 홈 */}
+        <Route path="/" element={<Home />} />
+
+        {/* 스터디 생성 */}
+        <Route path="/study/new/subject" element={<Challenge />} />
       </Routes>
     </Router>
   );
